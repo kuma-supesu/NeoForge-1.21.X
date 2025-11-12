7843e1a6b754bae620e824336b0c4eddf54f843f
kuma-supesu <35967447+kuma-supesu@users.noreply.github.com>
2025-11-12 21:45:46 +0100

    Adding Magic block Convert Bismuth into diamond and dandelion to rose bush

--- src/main/java/net/kuma/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/kuma/tutorialmod/block/ModBlocks.java
@@ -1,6 +1,7 @@
 package net.kuma.tutorialmod.block;
 
 import net.kuma.tutorialmod.TutorialMod;
+import net.kuma.tutorialmod.block.custom.MagicBlock;
 import net.kuma.tutorialmod.item.ModItems;
 import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
@@ -30,6 +31,9 @@ public class ModBlocks {
     public static final DeferredBlock<Block> BISMUTH_DEEPSLATE_ORE = registerBlock("bismuth_deepslate_ore",
             () -> new DropExperienceBlock(UniformInt.of(3, 6),
                     BlockBehaviour.Properties.of().strength(4f).requiresCorrectToolForDrops().sound(SoundType.DEEPSLATE)));
+
+    public static final DeferredBlock<Block> MAGIC_BLOCK = registerBlock("magic_block",
+            () -> new MagicBlock(BlockBehaviour.Properties.of().strength(2f).requiresCorrectToolForDrops()));
 
 
     private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
