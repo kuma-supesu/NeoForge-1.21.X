d00604848814e955a29261df71fb6a3dc5491d06
kuma-supesu <35967447+kuma-supesu@users.noreply.github.com>
2025-11-12 18:57:16 +0100

    Adding creative tabs

--- src/main/java/net/kuma/tutorialmod/TutorialMod.java
+++ src/main/java/net/kuma/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.kuma.tutorialmod;
 
 import net.kuma.tutorialmod.block.ModBlocks;
+import net.kuma.tutorialmod.item.ModCreativeModeTabs;
 import net.kuma.tutorialmod.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
@@ -36,6 +37,8 @@ public class TutorialMod {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        ModCreativeModeTabs.register(modEventBus);
 
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
