c444193930dfa34cfb2f95bd9da2a85dbf5d015c
kuma-supesu <35967447+kuma-supesu@users.noreply.github.com>
2025-11-12 23:09:52 +0100

    Adding tags

--- src/main/java/net/kuma/tutorialmod/block/custom/MagicBlock.java
+++ src/main/java/net/kuma/tutorialmod/block/custom/MagicBlock.java
@@ -1,6 +1,7 @@
 package net.kuma.tutorialmod.block.custom;
 
 import net.kuma.tutorialmod.item.ModItems;
+import net.kuma.tutorialmod.util.ModTags;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
@@ -36,7 +37,7 @@ public class MagicBlock extends Block {
     @Override
     public void stepOn(Level level, BlockPos pos, BlockState state, Entity entity) {
         if (entity instanceof ItemEntity itemEntity) {
-            if (itemEntity.getItem().getItem() == ModItems.RAW_BISMUTH.get()) {
+            if (isValidItem(itemEntity.getItem())) {
                 itemEntity.setItem(new ItemStack(Items.DIAMOND, itemEntity.getItem().getCount()));
             }
 
@@ -46,6 +47,10 @@ public class MagicBlock extends Block {
         }
 
         super.stepOn(level, pos, state, entity);
+    }
+
+    private boolean isValidItem(ItemStack item) {
+        return item.is(ModTags.Items.TRANSFORMABLE_ITEMS);
     }
 
     @Override
