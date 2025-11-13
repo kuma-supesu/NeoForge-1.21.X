ce4faac20931ff81b5a98964b03274cc8d488ec1
kuma-supesu <35967447+kuma-supesu@users.noreply.github.com>
2025-11-13 22:20:40 +0100

    Adding Armor horse

--- src/main/java/net/kuma/tutorialmod/datagen/ModItemModelProvider.java
+++ src/main/java/net/kuma/tutorialmod/datagen/ModItemModelProvider.java
@@ -50,6 +50,8 @@ public class ModItemModelProvider extends ItemModelProvider {
         basicItem(ModItems.FROSTFIRE_ICE.get());
         basicItem(ModItems.CHISEL.get());
 
+        basicItem(ModItems.BISMUTH_HORSE_ARMOR.get());
+
         buttonItem(ModBlocks.BISMUTH_BUTTON, ModBlocks.BISMUTH_BLOCK);
         fenceItem(ModBlocks.BISMUTH_FENCE, ModBlocks.BISMUTH_BLOCK);
         wallItem(ModBlocks.BISMUTH_WALL, ModBlocks.BISMUTH_BLOCK);
