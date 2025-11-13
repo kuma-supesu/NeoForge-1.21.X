b1c0c79a5cccd84f88d0e5ada867d4c4bf7631cf
kuma-supesu <35967447+kuma-supesu@users.noreply.github.com>
2025-11-13 17:50:15 +0100

    Adding Hammer

--- src/main/java/net/kuma/tutorialmod/item/ModItems.java
+++ src/main/java/net/kuma/tutorialmod/item/ModItems.java
@@ -2,6 +2,7 @@ package net.kuma.tutorialmod.item;
 
 import net.kuma.tutorialmod.TutorialMod;
 import net.kuma.tutorialmod.item.custom.ChiselItem;
+import net.kuma.tutorialmod.item.custom.HammerItem;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.*;
 import net.neoforged.bus.api.IEventBus;
@@ -56,6 +57,10 @@ public class ModItems {
     public static final DeferredItem<HoeItem> BISMUTH_HOE = ITEMS.register("bismuth_hoe",
             () -> new HoeItem(ModToolTiers.BISMUTH, new Item.Properties()
                     .attributes(HoeItem.createAttributes(ModToolTiers.BISMUTH, 0F, -3.4f))));
+
+    public static final DeferredItem<HammerItem> BISMUTH_HAMMER = ITEMS.register("bismuth_hammer",
+            () -> new HammerItem(ModToolTiers.BISMUTH, new Item.Properties()
+                    .attributes(PickaxeItem.createAttributes(ModToolTiers.BISMUTH, 7F, -3.5f))));
 
 
     public static void register(IEventBus eventBus) {
